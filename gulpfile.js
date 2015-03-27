97bc0c5c07f1b2acbc4a8bd64817ca50f94fd3d7
Christos Georgiou <xris.georgiou@gmail.com>
2015-03-27 21:57:22 +0200

    setup sass

--- gulpfile.js
+++ gulpfile.js
@@ -17,11 +17,11 @@ gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
     .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    // .pipe(minifyCss({
+    //   keepSpecialComments: 0
+    // }))
+    // .pipe(rename({ extname: '.min.css' }))
+    // .pipe(gulp.dest('./www/css/'))
     .on('end', done);
 });
 
